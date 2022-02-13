a57d62a5f0d6442f86caae427aab464552ed899b
RomdhoniZidane20 <73183067+RomdhoniZidane20@users.noreply.github.com>
2022-02-14 01:23:34 +0700

    Update login-script.js
    Mengganti comment dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,45 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/ 
 const loginFormElement = document.querySelector('#loginForm');
+/** 
+* Membuat variabel inputEmailElement dan inputPassword untuk tampilan input email dan password.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
-
-const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
-
+/** 
+* Membuat variabel expectedEmail dan expectedPassword untuk tampilan input email dab password.
+* @constant {string}
+*/
+const expectedEmail = 'romdhonizidane@gmail.com';
+const expectedPassword = 'icH12o';
+/** 
+* Menambahkan action pada click button.
+* @constant {string}
+*/
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    event.preventDefault();
+/** 
+* Memberi nilai variable email dan password dari input.
+* @constant {string}
+*/
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+/** 
+* Kondisi jika email dan password sesuai.
+* @constant {HTMLElement}
+*/
+    if (email == expectedEmail && password == expectedPassword){
+      goToHome();
+    }
+/** 
+* Kondisi jika email dan password tidak sesuai.
+* @constant {HTMLElement}
+*/
+    else {
+      showPopUp();
+    }
 });
